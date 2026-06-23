74359a1b875ff5b309a32bd63e1ac827e2b54b15
afrid2004 <mdfaisalafrid@gmail.com>
2026-06-23 21:38:23 +0600

    added slider in home page

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -6,3 +6,19 @@ const MobileMenuToggle = () => {
   mobileMenuModal.classList.toggle("menu-toogler");
   menuSidebar.classList.toggle("menu-sidebar-toggle");
 };
+
+// swipper js
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
