8c736ff3f78659e92e425ff85bcda1bc96322dea
afrid2004 <mdfaisalafrid@gmail.com>
2026-06-24 20:56:31 +0600

    added header scroll animation

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -22,3 +22,29 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// on scroll animation scripts
+let aboutSection = document.querySelector("#about"),
+  header = document.querySelector("#header"),
+  logo = document.querySelector("#logo");
+
+const animationTriggerFunction = (
+  targetSection,
+  currentSection,
+  activeClass,
+) => {
+  let observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        currentSection.classList.add(activeClass);
+        logo.src = "/assets/images/logo_black.png";
+      } else {
+        currentSection.classList.remove(activeClass);
+        logo.src = "/assets/images/logo_white.png";
+      }
+    });
+  });
+  observer.observe(targetSection);
+};
+
+animationTriggerFunction(aboutSection, header, "scroll-header");
